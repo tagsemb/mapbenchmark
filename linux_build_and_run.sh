5c5ede7ef62b39ed739a56dffde5b80e684cf49c
Thor Arne <tags@myvr-software.com>
2023-09-27 12:54:39 +0200

    g++ optimization flag

--- linux_build_and_run.sh
+++ linux_build_and_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 outfile=linux_x64/MapBenchmark
 mkdir -p linux_x64
-g++ -std=c++17 -march=x86-64 -Wno-format MapBenchmark.cpp -o $outfile && $outfile
+g++ -std=c++17 -march=x86-64 -O3 -Wno-format MapBenchmark.cpp -o $outfile && $outfile
